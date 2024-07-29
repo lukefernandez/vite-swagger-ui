8511ed398f1542fb99fdcd59ccdcc4f916ce9e8a
Luke Fernandez <8265202+lukefernandez@users.noreply.github.com>
2024-07-29 14:16:47 -0700

    add Swagger UI configured with JSON and YAML examples

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <h1>Vite + Swagger UI</h1>
-  </div>
-`;
+import SwaggerUI from "swagger-ui";
+import SwaggerUIStandalonePreset from "swagger-ui/dist/swagger-ui-standalone-preset";
+import "swagger-ui/dist/swagger-ui.css";
+
+SwaggerUI({
+  urls: [
+    { url: "./specs/petstore.json", name: "JSON — Pet Store" },
+    {
+      url: "./specs/petstore-expanded.yaml",
+      name: "YAML — Pet Store Expanded",
+    },
+  ],
+  dom_id: "#app",
+  deepLinking: true,
+  presets: [SwaggerUI.presets.apis, SwaggerUIStandalonePreset],
+  layout: "StandaloneLayout",
+});
