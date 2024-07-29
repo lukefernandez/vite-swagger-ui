7038d0e0f90b0309e56907e1ea164f0b795ded26
Luke Fernandez <8265202+lukefernandez@users.noreply.github.com>
2024-07-29 16:00:33 -0700

    Add Swagger UI configuration and README (#1)

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <h1>Vite + Swagger UI</h1>
-  </div>
-`;
+import SwaggerUI from "swagger-ui";
+import SwaggerUIStandalonePreset from "swagger-ui/dist/swagger-ui-standalone-preset";
+import "swagger-ui/dist/swagger-ui.css";
+
+SwaggerUI({
+  urls: [
+    { url: "./specs/petstore.json", name: "Pet Store — JSON" },
+    {
+      url: "./specs/petstore.yaml",
+      name: "Pet Store — YAML",
+    },
+  ],
+  dom_id: "#app",
+  deepLinking: true,
+  presets: [SwaggerUI.presets.apis, SwaggerUIStandalonePreset],
+  layout: "StandaloneLayout",
+});
---
+++ src/swagger-ui.d.ts
@@ -0,0 +1,2 @@
+declare module "swagger-ui";
+declare module "swagger-ui/dist/swagger-ui-standalone-preset";
